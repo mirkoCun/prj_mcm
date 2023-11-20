e03cc2abca334c11f9da03831a2fb94cbe78ea8b
mirkoCun <mirko.cunaccia@buonarroti.tn.it>
2023-11-20 12:21:52 +0100

    finisch_02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,8 @@ public class Main {
         CoppiaNumeri coppia1_obj = new CoppiaNumeri (n1,n2);
         coppia1_obj.setN1(n1);
         coppia1_obj.setN2(n2);
+        System.out.println("N1: " + coppia1_obj.getN1());
+        System.out.println("N2: " + coppia1_obj.getN2());
         System.out.println("il minimo comune multiplo tra i due numeri è " + coppia1_obj.mcm());
         System.out.println("BUBU Milano");
     }
